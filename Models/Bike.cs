65fb886d35f4dfc1264ba42b94f11b50b4133568
Raphael MOUROT-PELADE <raphael.mourot-pelade@epita.fr>
2021-03-21 19:41:07 +0100

    class initialisation

---
+++ Models/Bike.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DublinBikesApi.Models
+{
+    public class Bike
+    {
+        int number { get; set; }
+        string contractName { get; set; }
+        string name { get; set; }
+        string adress { get; set; }
+        decimal latitude { get; set; }
+        decimal longitude { get; set; }
+        bool banking { get; set; }
+        int AvailableBikes { get; set; }
+        int AvailableStands { get; set; }
+        int Capacity { get; set; }
+        string status { get; set; }
+
+    }
+}
