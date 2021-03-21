6b52469a8105aa2d61e904bbcf3d604cd88b507e
Baptiste Nehmé <baptiste.nehme@epita.fr>
2021-03-21 19:42:42 +0000

    Context Added

---
+++ Data/Context.cs
@@ -0,0 +1,15 @@
+﻿using DublinBikesApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DublinBikesApi.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+        public DbSet<Bike> Bike { get; set; }
+    }
+}
